a919e485dd229b68ef88f4067a1a9ffe0b68b5d9
xuxu <xuxucainiao001@163.com>
2018-10-18 20:22:50 +0800

    10.18

--- business/src/main/java/com/pgy/ups/account/business/factory/proofread/ProofreadHandlerFactory.java
+++ business/src/main/java/com/pgy/ups/account/business/factory/proofread/ProofreadHandlerFactory.java
@@ -2,7 +2,7 @@ package com.pgy.ups.account.business.factory.proofread;
 
 import java.io.InputStream;
 
-import com.pgy.ups.account.business.factory.interfaces.BusinessFactory;
+import com.pgy.ups.account.business.factory.BusinessFactory;
 import com.pgy.ups.account.business.handler.proofread.ProofreadHandler;
 
 interface ProofreadHandlerFactory<T,R> extends BusinessFactory<ProofreadHandler<T,R>> {
