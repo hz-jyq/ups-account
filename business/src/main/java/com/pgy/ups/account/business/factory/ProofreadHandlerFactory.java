e366355bf41ffd08768d055a737aa842eb5db7e2
xuxu <xuxucainiao001@163.com>
2018-10-23 14:36:12 +0800

    10.23

--- business/src/main/java/com/pgy/ups/account/business/factory/ProofreadHandlerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.pgy.ups.account.business.factory;
-
-import com.pgy.ups.account.business.handler.ProofreadHandler;
-
-public class ProofreadHandlerFactory implements BusinessFactory<ProofreadHandler>{
-         
-
-}
