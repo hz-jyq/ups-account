f692f5258929ab189da498fb4c259235fda3569c
xuxu <xuxucainiao001@163.com>
2018-10-26 18:22:52 +0800

    10.26

--- facade/src/main/java/com/pgy/ups/account/facade/from/ProofreadErrorForm.java
+++ facade/src/main/java/com/pgy/ups/account/facade/from/ProofreadErrorForm.java
@@ -2,5 +2,10 @@ package com.pgy.ups.account.facade.from;
 
 public class ProofreadErrorForm extends  AbstractPageForm<ProofreadErrorForm>{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 
 }
