fd9565b8b9463b7b4b6ce75147b5d8e66ac75a76
jinyuanqian <jinyuanqian@letto8.com>
2018-10-26 19:34:32 +0800

    add 代码

--- facade/src/main/java/com/pgy/ups/account/facade/from/ProofreadErrorForm.java
+++ facade/src/main/java/com/pgy/ups/account/facade/from/ProofreadErrorForm.java
@@ -2,5 +2,43 @@ package com.pgy.ups.account.facade.from;
 
 public class ProofreadErrorForm extends  AbstractPageForm<ProofreadErrorForm>{
 
+    private  String channel;
 
+    private String fromSystem;
+
+    private  String proofreadType;
+
+    private String errorType;
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getFromSystem() {
+        return fromSystem;
+    }
+
+    public void setFromSystem(String fromSystem) {
+        this.fromSystem = fromSystem;
+    }
+
+    public String getProofreadType() {
+        return proofreadType;
+    }
+
+    public void setProofreadType(String proofreadType) {
+        this.proofreadType = proofreadType;
+    }
+
+    public String getErrorType() {
+        return errorType;
+    }
+
+    public void setErrorType(String errorType) {
+        this.errorType = errorType;
+    }
 }
