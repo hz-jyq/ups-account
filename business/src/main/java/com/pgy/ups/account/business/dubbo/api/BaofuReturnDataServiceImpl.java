32071e972fa60a6c38d48eef3e3dc2cccb718b28
xuxu <xuxucainiao001@163.com>
2018-11-01 12:05:17 +0800

    11.1

--- business/src/main/java/com/pgy/ups/account/business/dubbo/api/BaofuReturnDataServiceImpl.java
+++ business/src/main/java/com/pgy/ups/account/business/dubbo/api/BaofuReturnDataServiceImpl.java
@@ -1,6 +1,7 @@
 package com.pgy.ups.account.business.dubbo.api;
 
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.pgy.ups.account.business.dao.mapper.BaofuReturnDataDao;
 import com.pgy.ups.account.facade.dubbo.api.BaofuReturnDataService;
 import com.pgy.ups.account.facade.from.ExcelForm;
@@ -9,6 +10,7 @@ import com.pgy.ups.account.facade.model.proofread.BaoFuModelReturn;
 import javax.annotation.Resource;
 import java.util.List;
 
+@Service
 public class BaofuReturnDataServiceImpl implements BaofuReturnDataService {
 
     @Resource
