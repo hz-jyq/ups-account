21c04335f8972294fccd4af5bae9633584f74f19
xuxu <xuxucainiao001@163.com>
2018-11-01 14:29:07 +0800

    11.1

--- facade/src/main/java/com/pgy/ups/account/facade/dubbo/api/ProofreadErrorService.java
+++ facade/src/main/java/com/pgy/ups/account/facade/dubbo/api/ProofreadErrorService.java
@@ -1,13 +1,12 @@
 package com.pgy.ups.account.facade.dubbo.api;
 
 
+import java.util.List;
+
 import com.pgy.ups.account.facade.from.ExcelForm;
 import com.pgy.ups.account.facade.from.PageInfo;
 import com.pgy.ups.account.facade.from.ProofreadErrorForm;
 import com.pgy.ups.account.facade.model.proofread.ProofreadError;
-import com.pgy.ups.account.facade.model.proofread.ProofreadSuccess;
-
-import java.util.List;
 
 
 public interface ProofreadErrorService {
